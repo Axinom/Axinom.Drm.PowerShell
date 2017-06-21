757b0765ba88529c98455dda63e3319001517a36
Sander Saares <saares@axinom.com>
2017-06-21 13:31:59 +0300

    Empty projects

---
+++ Resources/ComponentAssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyCompany("Axinom")]
+[assembly: AssemblyProduct("Axinom DRM PowerShell module")]
+
+// This is the real version number, used in the delivery process, NuGet packages and for display purposes.
+[assembly: AssemblyFileVersion("1.0.0")]
+
+// Only use major version here, with others kept at zero, for correct assembly binding logic.
+[assembly: AssemblyVersion("1.0.0")]
---
+++ Resources/ComponentConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Axinom.Drm.PowerShell
+{
+	static class ComponentConstants
+	{
+		// Will be replaced with real version string (AssemblyInfo number + build parameters) on automated build.
+		public const string VersionString = "__BUILD_BUILDNUMBER__";
+	}
+}
