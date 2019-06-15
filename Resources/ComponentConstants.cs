15a47f808bec82c6751843b2ee40d345935cc659
Sander Saares <saares@axinom.com>
2019-06-15 12:53:28 +0300

    Simplify project structure

--- Resources/ComponentConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Axinom.Drm.PowerShell
-{
-	static class ComponentConstants
-	{
-		// Will be replaced with real version string (AssemblyInfo number + build parameters) on automated build.
-		public const string VersionString = "__BUILD_BUILDNUMBER__";
-	}
-}
